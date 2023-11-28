a13db7eaf27aa57be1cd7f6afcf73797ba226cd9
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2023-11-28 02:01:31 -0300

    added data fetching

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
-import { Montserrat } from 'next/font/google';
+import { Montserrat, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export const montserrat = Montserrat({
   subsets: ['latin'],
