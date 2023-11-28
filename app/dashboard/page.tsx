044ffcdc844178d9f3a09ef133379072ca0c85a4
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2023-11-28 01:30:33 -0300

    feat: added first layouts

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const DashBoardPage = () => {
+  return <p>DashBoard Pagee</p>;
+};
+
+export default DashBoardPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+
+        <footer className="absolute bottom-0 flex h-16 w-full items-center justify-center bg-gray-900 text-white">
+          <p className="text-sm">Made with ❤️ by Vercel</p>
+        </footer>
+      </body>
     </html>
   );
 }
